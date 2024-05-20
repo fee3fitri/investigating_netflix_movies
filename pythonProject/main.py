bd22dd157ddcf73ca9fb24b7eee9628a40f36589
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-05-20 12:19:36 -0700

    Change the short movies to netflix movies data for the loop

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,3 +1,4 @@
+# Importing pandas and matplotlib
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -8,7 +9,7 @@ file_path = 'netflix_data.csv'
 netflix_df = pd.read_csv(file_path, index_col= 0)
 
 # Filter data to remove TV shows
-netflix_subset = netflix_df[netflix_df['type'] != 'TV Show']
+netflix_subset = netflix_df[netflix_df['type'] == 'Movie']
 
 # Only show certain columns from netflix_subset
 netflix_movies = netflix_subset.loc[:, ["title", "country", "genre", "release_year", "duration"]]
@@ -19,18 +20,21 @@ short_movies = netflix_movies[netflix_movies['duration'] < 60]
 # Assign colors to genre group
 colors = []
 
-for lab, row in short_movies.iterrows():
-    if "Children" in row['genre']:
+for lab, row in netflix_movies.iterrows():
+    if row['genre'] == "Children":
         colors.append("blue")
-    elif "Documentaries" in row['genre']:
+    elif row['genre'] == "Documentaries":
         colors.append('yellow')
-    elif "Stand-Up" in row['genre']:
+    elif row['genre'] == "Stand-Up":
         colors.append('green')
     else:
         colors.append('red')
 
 # Matplotlib figure object
-fig = plt.scatter(short_movies['release_year'], short_movies['duration'], c=colors)
+fig = plt.figure(figsize=(12, 8))
+
+# Create scatter plot
+plt.scatter(netflix_movies['release_year'], netflix_movies['duration'], c=colors)
 
 # Add labels
 plt.xlabel("Release year")
@@ -45,4 +49,4 @@ pd.set_option('display.max_rows', None)
 plt.show()
 
 # Are we certain that movies are getting shorter?
-answer = "yes"
+answer = "no"
